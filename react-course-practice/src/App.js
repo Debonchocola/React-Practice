b5afc18f70f5778d6c7889bb576a845de1c4a9de
Debonchocola <pub10@naver.com>
2023-02-27 00:30:49 +0900

    Feat:컴포넌트 생성 및 분리

--- react-course-practice/src/App.js
+++ react-course-practice/src/App.js
@@ -1,7 +1,21 @@
+import Expenses from "./components/Expenses";
+
 function App() {
+  const expenses = [
+    {
+      id: "el1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    { id: "el2", title: "1", amount: 55.2, date: new Date(2021, 2, 28) },
+    { id: "el3", title: "2", amount: 29.7, date: new Date(2022, 12, 20) },
+    { id: "el4", title: "3", amount: 76, date: new Date(2019, 5, 10) },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
